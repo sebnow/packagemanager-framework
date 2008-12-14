92bc243f656a2757d082cf8da8be59879aed87ea
Sebastian Nowicki <sebnow@gmail.com>
2008-12-14 20:09:52 +0900

    Add PMRepository class
    Signed-off-by: Sebastian Nowicki <sebnow@gmail.com>
    
    The sync transaction does not appear to be necessary or even relevant to
    refreshing a database. Pacman code confirms this.
    
    Signed-off-by: Sebastian Nowicki <sebnow@gmail.com>

---
+++ Classes/PMRepository.m
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2008 Sebastian Nowicki <sebnow@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#import "PMRepository.h"
+#import <alpm.h>
+
+@implementation PMRepository
+@synthesize servers = _servers;
+
+- (id) initWithName:(NSString *)aName
+{
+	return [self initWithName:aName servers:nil];
+}
+
+- (id) initWithName:(NSString *)aName servers:(NSArray *)theServers
+{
+	self = [super init];
+	if(self != nil) {
+		_database = alpm_db_register_sync([aName UTF8String]);
+		if(theServers == nil) {
+			_servers = [NSMutableArray new];
+		} else {
+			_servers = [[NSMutableArray alloc] initWithArray:theServers];
+		}
+	}
+	return self;
+}
+
+- (void) dealloc
+{
+	[_servers release];
+	[super dealloc];
+}
+
+- (void) addServerWithURL:(NSURL *)theURL
+{
+	const char *urlUTF8 = [[theURL path] UTF8String];
+	if(alpm_db_setserver(_database, urlUTF8) == 0) {
+		[_servers addObject:theURL];
+	}
+}
+
+- (void) refresh
+{
+	alpm_db_update(0, _database);
+}
+
+- (void) forceRefresh
+{
+	alpm_db_update(1, _database);
+}
+
+@end
